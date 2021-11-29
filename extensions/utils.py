b0fa12cc016ea27a60d5b7b8727c5cffecd3cfa5
SnakryGoblin092 <orsosemil092@gmail.com>
2021-11-29 01:10:55 +0100

    Implement server stats in utils extension

--- extensions/utils.py
+++ extensions/utils.py
@@ -1,6 +1,6 @@
 import hikari
 import lightbulb
-import datetime
+from datetime import datetime, timedelta
 import asyncio
 from lightbulb import commands, context
 
@@ -17,16 +17,15 @@ async def clear(ctx: context.Context) -> None:
     clear_count = ctx.options.count if (ctx.options.count is not None) else 1
 
     async with target_channel.trigger_typing():
-        # Don't try to delete past two weeks
-        now = datetime.datetime.utcnow()
-        two_weeks_ago = now - datetime.timedelta(weeks=2)
+        now = datetime.utcnow()
+        two_weeks_ago = now - timedelta(weeks=2)
         history = await target_channel.fetch_history(after=two_weeks_ago)
-        history_trimmed = history[-clear_count-1:]
+        history_limited = history[-clear_count-1:]
 
-        deleted_messages_count = len(history_trimmed)
-        await target_channel.delete_messages(history_trimmed)
+        deleted_messages_count = len(history_limited)
+        await target_channel.delete_messages(history_limited)
     
-    sent_message = await ctx.get_channel().respond(f"Deleted {deleted_messages_count} messages!")
+    sent_message = await ctx.respond(f"Deleted {deleted_messages_count} messages!")
     await asyncio.sleep(10)
     await sent_message.delete()
 
@@ -39,6 +38,50 @@ async def nickname(ctx: context.Context) -> None:
     nick = ctx.options.nickname if (ctx.options.nickname is not None) else ""
     await ctx.member.edit(nick=nick)
 
+
+@utils_plugin.command
+@lightbulb.command("serverstats", "Get stats of the server.")
+@lightbulb.implements(commands.SlashCommand)
+async def stats(ctx: context.Context) -> None:
+    guild = ctx.get_guild()
+    roles = await guild.fetch_roles()
+    usable_roles = roles[1:]
+    
+    members = guild.get_members()
+    dicts = {}
+    
+    for role in usable_roles:
+        dicts[role.id] = 0
+
+    for member in members:
+        members_roles = guild.get_member(member).role_ids
+        for role_id in members_roles:
+            if role_id in dicts.keys():
+                dicts[role_id] += 1
+
+    embed = (
+        hikari.Embed(
+            title=f"{guild.name}",
+            description=f"Server Stats",
+            colour=0xFF793B,
+            timestamp=datetime.now().astimezone(),
+        )
+        .set_footer(
+            text=f"Requested by {ctx.member.display_name}",
+            icon=ctx.member.avatar_url,
+        )
+        .set_thumbnail(guild.banner_url)
+    )
+
+    for key, value in dicts.items():
+        embed.add_field(
+            f"{value} has",
+            f"{guild.get_role(key).mention}",
+            inline=True
+        )
+
+    await ctx.respond(embed)
+        
 
 def load(bot: lightbulb.BotApp) -> None:
     bot.add_plugin(utils_plugin)
