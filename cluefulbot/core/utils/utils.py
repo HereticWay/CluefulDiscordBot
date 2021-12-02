5f5bb40831d029c3bd56da3eddaab14d174f74f2
HereticWay <vpleft@gmail.com>
2021-12-02 20:39:56 +0100

    add docsstring and return type to is_prefix_command util;

--- cluefulbot/core/utils/utils.py
+++ cluefulbot/core/utils/utils.py
@@ -1,5 +1,13 @@
 from lightbulb import context
 
 
-def is_prefix_command(ctx: context.Context):
+def is_prefix_command(ctx: context.Context) -> bool:
+    """Returns if the given context is using prefix commands
+
+    Args:
+        ctx (context.Context): the context of the command
+
+    Returns:
+        bool: True if the context is a PrefixCommand. False if it's a SlashCommand.
+    """
     return ctx.interaction is None
