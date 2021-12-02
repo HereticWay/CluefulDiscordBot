5c0c386e51c7e84a50ee2bf0b119f67bbec40665
HereticWay <vpleft@gmail.com>
2021-12-02 20:21:16 +0100

    implement parse_settings() in main.py;

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 #!/usr/bin/python3
 from cluefulbot.core import cluefulbot
+import typing as t
 import os
-
-GUILD_ID = int(os.getenv("GUILD_ID"))
-TOKEN = os.getenv('TOKEN')
+import sys
 
 
-def main():
+def parse_settings() -> t.Tuple[str, int]:
+    token: str = os.getenv("TOKEN")
+    guild_id: str = os.getenv("GUILD_ID")
+    if not token or not guild_id:
+        print("Please specify both TOKEN and GUILD_ID environment variables!", file=sys.stderr)
+        quit(1)
+
+    try:
+        guild_id_int = int(guild_id)
+    except ValueError:
+        print("GUILD_ID environment variable should be an integer!", file=sys.stderr)
+        quit(1)
+
+    return token, guild_id_int
+
+
+def main() -> None:
     if os.name != "nt":
         import uvloop
         uvloop.install()
 
-    my_bot = cluefulbot.CluefulBot(token=TOKEN, guild_id=GUILD_ID)
+    token, guild_id = parse_settings()
+    my_bot = cluefulbot.CluefulBot(token=token, guild_id=guild_id)
     my_bot.run()
 
 
