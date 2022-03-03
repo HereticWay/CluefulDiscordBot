59608b536ab8c01335f7f1c21eda0dfc791b4220
HereticWay <vpleft@gmail.com>
2022-03-03 04:26:05 +0100

    use config util in CluefulBot; simplify main;

--- cluefulbot/core/cluefulbot.py
+++ cluefulbot/core/cluefulbot.py
@@ -1,14 +1,16 @@
 import hikari
 import lightbulb
+from utils.config import *
 
 
 class CluefulBot(lightbulb.BotApp):
-    def __init__(self, token: str, guild_id: int) -> None:
+    def __init__(self) -> None:
         lightbulb.BotApp.__init__(
             self,
-            token=token,
+            token=get_token(),
             prefix="$",
             banner=None,
-            intents=hikari.Intents.ALL, default_enabled_guilds=(guild_id,)
+            intents=hikari.Intents.ALL, default_enabled_guilds=(get_guild_id(),)
         )
+
         self.load_extensions_from("./cluefulbot/core/extensions", recursive=True, must_exist=True)
--- main.py
+++ main.py
@@ -1,24 +1,6 @@
 #!/usr/bin/python3
 from cluefulbot.core import cluefulbot
-import typing as t
 import os
-import sys
-
-
-def parse_settings() -> t.Tuple[str, int]:
-    token: str = os.getenv("TOKEN")
-    guild_id: str = os.getenv("GUILD_ID")
-    if not token or not guild_id:
-        print("Please specify both TOKEN and GUILD_ID environment variables!", file=sys.stderr)
-        quit(1)
-
-    try:
-        guild_id_int = int(guild_id)
-    except ValueError:
-        print("GUILD_ID environment variable should be an integer!", file=sys.stderr)
-        quit(1)
-
-    return token, guild_id_int
 
 
 def main() -> None:
@@ -26,8 +8,7 @@ def main() -> None:
         import uvloop
         uvloop.install()
 
-    token, guild_id = parse_settings()
-    my_bot = cluefulbot.CluefulBot(token=token, guild_id=guild_id)
+    my_bot = cluefulbot.CluefulBot()
     my_bot.run()
 
 
